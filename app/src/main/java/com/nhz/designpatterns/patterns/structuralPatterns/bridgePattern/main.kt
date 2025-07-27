e291e23bd863078440be834855165d30d1e0543b
Naing Htet Zaw <nainghtetzaw20101@gmail.com>
2025-07-27 16:33:24 +0700

    Bridge pattern (DEMO)

--- app/src/main/java/com/nhz/designpatterns/patterns/structuralPatterns/bridgePattern/main.kt
+++ app/src/main/java/com/nhz/designpatterns/patterns/structuralPatterns/bridgePattern/main.kt
@@ -1,5 +1,13 @@
 package com.nhz.designpatterns.patterns.structuralPatterns.bridgePattern
 
 fun main() {
-    
+    val service = NotificationService()
+
+    service.send(
+        platForm = "ios",
+        notificationType = "sms",
+        title = "Sending iOS SMS noti!",
+        message = "Hello!",
+        phone = "0612345678"
+    )
 }
